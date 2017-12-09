dcf3d14d7e90423a6e89f92d9f16d015020fb6b2
Gareth M <gareth@local>
2017-12-09 18:58:41 +0000

    Pair of tests for the dict-searcher

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,26 @@
-from unittests import TestCase
+import unittest
+
+from scraper.parse import key_value
 
 
-class ScraperTest(TestCase):
-    pass
+class DictTest(unittest.TestCase):
+    def test_simple_dict(self):
+        d = {'name': 'bob',
+             'color': 'green'}
+        self.assertEquals(key_value(d, 'color'), 'green')
+
+    def test_nested_dict(self):
+        d = {'properties': {'initial':
+                            {'name': 'bob',
+                             'color': 'green'},
+                            'final':
+                            {'name': 'frank',
+                             'color': 'chartreuse'}},
+             'spans': {'2017': {'february': '1d93'},
+                       '2016': {'march': '9jf'}}
+             }
+        self.assertEquals(key_value(d, 'march'), '9jf')
+
+
+if __name__ == '__main__':
+    unittest.main()
