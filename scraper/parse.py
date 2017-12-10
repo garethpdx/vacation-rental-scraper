ff1dab85d2d9c0b23b5a56fabe028805e71642e5
Gareth M <gareth@local>
2017-12-10 17:12:54 +0000

    When searching nested dicts, Do a better job of skipping values that aren't dicts

--- scraper/parse.py
+++ scraper/parse.py
@@ -1,31 +1,38 @@
-"""
-
-"""
-
-import json, pprint, codecs
-from bs4 import BeautifulSoup
+" Tools for nested dictionaries of unknown structure "
 
 
+def nested_get(nested_dict, needle):
+    """
+    recursively search nested dictionaries for key 'needle' and return value
+    """
+    try:
+        for k, v in nested_dict.items():
+            if k == needle:
+                return v
+            value = nested_get(v, needle)
+            if value is not None:
+                return value
+    except AttributeError:
+        # ignore values that don't quack like a dict
+        pass
 
-def key_path(d, needle, path=None):
+
+def key_path(nested_dict, needle, path=None):
+    """
+    recursively search nested dictionaries for a key named 'needle',
+    returning path to the key as a list
+    """
     if path is None:
         path = []
-    for key in d:
-        if key == needle:
-            path.insert(0, key)
-            return path
-        if isinstance(d[key], dict):
-            location =  key_path(d[key], needle, path)
-            if location:
-                location.insert(0, key)
+    try:
+        for k, v in nested_dict.items():
+            if k == needle:
+                path.insert(0, k)
                 return path
-
-
-def key_value(d, needle):
-    path = key_path(d, needle)
-    for key in path:
-        d = d.get(key)
-    return d
+            location = key_path(nested_dict[k], needle, path)
+            if location:
+                location.insert(0, k)
+                return path
     except AttributeError:
         # ignore values that don't quack like a dict
         pass
